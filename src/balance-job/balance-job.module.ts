116ae49332a7f37a3ca62b6c4241acc33c5620a1
Deepak Pathak <imdeepakpathak@gmail.com>
2025-02-02 23:50:14 +0530

    feat: monthly update worker

--- src/balance-job/balance-job.module.ts
+++ src/balance-job/balance-job.module.ts
@@ -2,14 +2,15 @@ import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { JobProducerService } from './job-producer.service';
 import { JobWorkerService } from './job-worker.service';
+import { QUEUE_NAME } from 'src/shared/util';
 
 @Module({
   imports: [
     BullModule.registerQueue({
-      name: 'monthly_users',
+      name: QUEUE_NAME.MONTHLY_USERS,
     }),
     BullModule.registerQueue({
-      name: 'daily_users',
+      name: QUEUE_NAME.DAILY_USERS,
     }),
   ],
   exports: [JobProducerService],
--- src/balance-job/job-worker.service.ts
+++ src/balance-job/job-worker.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Logger } from '@nestjs/common';
+import { Job } from 'bullmq';
+import { QUEUE_NAME } from 'src/shared/util';
+import { BalanceJobBatch } from './job-producer.service';
 
-@Injectable()
-export class JobWorkerService {}
+@Processor(QUEUE_NAME.MONTHLY_USERS, { concurrency: 4 })
+export class JobWorkerService extends WorkerHost {
+  private logger = new Logger(JobWorkerService.name);
+
+  constructor() {
+    super();
+  }
+
+  async process(job: Job<any, any, string>): Promise<any> {
+    this.logger.debug((job.data as BalanceJobBatch).userBatch.split(','));
+    return {};
+  }
+}
